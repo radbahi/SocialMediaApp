8179b61c1abad0210aeeafd6797bc41d73a1d88a
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-06-20 10:15:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const express = require("express");
+
+const app = express();
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running");
+});
