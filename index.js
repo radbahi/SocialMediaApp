a37859427da3804c7aa512e02a7b462935bd61d4
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-06-20 10:36:44 -0400

    user route and schema done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const express = require("express");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 const app = express();
 dotenv.config();
@@ -19,9 +21,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.send("welcome to homepage");
-});
+//routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running");
