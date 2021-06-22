428836632c78324f9d45891fadc90a92b2af1c6e
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-06-22 11:43:18 -0400

    working on login/register

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("USER ROUTERHBEJ");
+//update user
+router.put("/:id", async (req, res) => {
+  if ((req, body.userId === req.params.id || req.user.isAdmin)) {
+    if (req.body.password) {
+      try {
+        const salt = await brcrypt.genSalt(10);
+      } catch (error) {}
+    }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
 });
+//delete user
+
+//get user
+
+//follow a user
+
+//unfollow a user
 
 module.exports = router;
