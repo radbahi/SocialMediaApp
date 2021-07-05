4272e57ee31f7664e9dc2ab6bfa6c06d2e7d47e7
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-07-05 09:40:25 -0400

    rightbar styling done

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,33 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img
+            className="birthdayImg"
+            alt="birthdayImg"
+            src="/assets/gift.png"
+          />
+          <span className="birthdayText">Birthday today here</span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/heart.png"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Username here</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
